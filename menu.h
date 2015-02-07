6308aa266d43fe4ffa3c814dbb8a884e65b440d2
pereodinofzolos <edem4420@hotmail.com>
2015-02-07 18:48:48 -0500

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,18 @@
+/* menu Class header
+
+
+February 7 2015 */
+
+#indef MENU_H_
+#define MENU_H_
+
+Class menu {
+
+Protected:
+  menu.chooseView();
+  menu.displayMainMenu();
+  menu.terminate();
+
+};
+
+#endif
