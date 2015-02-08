520c2d0190850bbac9306d46784505f582b8a226
pereodinofzolos <edem4420@hotmail.com>
2015-02-07 19:24:04 -0500

    Update menu.cpp

--- View/src/menu.cpp
+++ View/src/menu.cpp
@@ -7,7 +7,19 @@ February 7th 2015
 #include <iostream>
 #include "menu.h"
 
-menu :: menuView (){
 
+
+
+// On mouseclick over a view button creates an instance of that type of view 
+void menu::chooseView(){
+  
+  
 }
 
+
+// Checks for a signal corresponding to the completion of the run or the user's request to terminate a view
+// "Deletes all instances of the view
+void menu::terminate(){
+  
+  
+}
