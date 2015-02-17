507f7327dfb39ebba7abd0738396cefbebb3b8ba
pereodinofzolos <edem4420@hotmail.com>
2015-02-17 00:39:43 -0500

    Create observer.cpp

---
+++ View/src/observer.cpp
@@ -0,0 +1,32 @@
+/*
+ * observer.cpp
+ *
+ *  Created on: Feb 17, 2015
+ *      Author: edem
+ */
+
+#include "observer.h"
+
+observer:: observer(){
+
+
+}
+
+bool observer::freeToRead(){
+	bool canread = true;
+
+	// stuff
+
+	return canread;
+}
+
+
+void observer::readState(){
+
+
+}
+
+observer::~observer(){
+
+
+}
