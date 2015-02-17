7beafdecba8548f2402dd17684035178a3fc0d17
pereodinofzolos <edem4420@hotmail.com>
2015-02-17 00:40:10 -0500

    Create view.cpp

---
+++ View/src/view.cpp
@@ -0,0 +1,67 @@
+/*
+ * view.cpp
+ *
+ *  Created on: Feb 17, 2015
+ *      Author: edem
+ */
+
+
+#include "view.h"
+
+view::view() {
+
+}
+
+void view::moveForward(){
+
+}
+
+void view::moveBackward(){
+
+}
+
+void view::moveLeft(){
+
+}
+
+void view::moveRight(){
+
+}
+
+void view::moveUp(){
+
+}
+
+void view::moveDown(){
+
+}
+
+void view::surface(){
+
+}
+
+void view::launchTor(){
+
+}
+
+void view::launchGra(){
+
+}
+
+void view::changeWinDim(){
+
+}
+
+void view::displayWin(){
+
+}
+
+void view::toMenu(){
+
+}
+
+
+
+view::~view(){
+
+}
