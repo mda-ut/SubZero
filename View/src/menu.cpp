ffd329df939d99854a3fca329e7a9bbf82929982
pereodinofzolos <edem4420@hotmail.com>
2015-02-17 18:20:16 -0500

    Capitalized class name & added con (de) structors

--- View/src/menu.cpp
+++ View/src/menu.cpp
@@ -9,9 +9,12 @@ February 7th 2015
 
 
 
+Menu::Menu(){
+  
+}
 
 // On mouseclick over a view button creates an instance of that type of view 
-void menu::chooseView(){
+void Menu::chooseView(){
   
   
 }
@@ -19,7 +22,12 @@ void menu::chooseView(){
 
 // Checks for a signal corresponding to the completion of the run or the user's request to terminate a view
 // "Deletes all instances of the view
-void menu::terminate(){
+void Menu::terminate(){
+  
+  
+}
+
+Menu::~Menu(){
   
   
 }
