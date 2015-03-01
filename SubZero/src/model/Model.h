40c9d623cf221a3fc9c4224f1344e2b6a26a5ac3
zackage <zack.tzeng@gmail.com>
2015-03-01 17:36:18 -0500

    Model.h - Modify the comments to follow javadoc style.

--- SubZero/src/model/Model.h
+++ SubZero/src/model/Model.h
@@ -8,29 +8,46 @@
 #ifndef MODEL_H_
 #define MODEL_H_
 
+#include <string>
+
+using namespace std;
+
 /**
  * This is an abstract class that represents the physical attributes of the sub.
  * It accepts input from cameras and FPGA and outputs to FPGA and
  * cameras (strictly speaking). Model contains references to the tools required
  * for the assets to run. Each Model inherits from observable and has a state.
  * Each Model has a filter.
+ *
+ * @author	Zack Tzeng
+ * @version	0.0
+ * @since	2014-03-01
  */
-
 class Model {
+
 private:
 	FilterManager* filterManager;
 	FilterFactory* filterFactory;
+
 public:
-	Model(FilterManager* fM);
-	Model(FilterManager* fM, FilterFactory* fF);
 	/**
-	 * Constructors for Model. There are overload for Model constructor in case FilterFactory
-	 * instance is not to be created.
+	 * This is the main constructor of a Model object.
+	 * @param	fM	This is the only parameter, a reference to FilterManager, to this Model constructor.
+	 */
+	Model(FilterManager* fM);
+
+	/**
+	 * This is the overload of the constructor of a Model object with one additional FilterFactory parameter.
+	 * @param	fM	This is the first parameter to the overload constructor.
+	 * @param	fF	This is the second parameter to the overload constructor if a FilterFactory object is created.
+	 */
+	Model(FilterManager* fM, FilterFactory* fF);
+
+	/**
+	 * This is the destructor of Model.
+	 * @return	Nothing.
 	 */
 	virtual ~Model();
-	/**
-	 * Deconstructor. yeah.
-	 */
 };
 
 #endif /* MODEL_H_ */
