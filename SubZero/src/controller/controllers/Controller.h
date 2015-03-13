04d8bc69ce25f922de7d738b4b8dcb27405bf374
cannon-edgar <edgarwang95@gmail.com>
2015-03-12 20:07:06 -0400

    added controller files
    -Stubbed Controller class

--- SubZero/src/controller/controllers/Controller.h
+++ SubZero/src/controller/controllers/Controller.h
@@ -1,17 +1,65 @@
 /*
  * Controller.h
  *
- *  Created on: Jan 7, 2015
- *      Author: mda
+ *  Created on: Mar 12, 2015
+ *      Author: ed
  */
 
-#ifndef CONTROLLER_H_
-#define CONTROLLER_H_
+#ifndef SRC_CONTROLLER_H_
+#define SRC_CONTROLLER_H_
 
-class Controller {
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class Controller{
 public:
-	Controller();
-	virtual ~Controller();
+    //constructor for the controller class
+    Controller(Model *_modelpointer, Mode *_modepointer, string _command){
+        sub_model=_modelpointer;
+        current_mode=_modepointer;
+        command=_command;
+    }
+    //Destructor to free pointers
+    ~Controller(){
+        free(sub_model);
+        free(current_mode);
+    }
+    
+    //movement commands
+    void move_forward();
+    void move_backward();
+    void move_right();
+    void move_left();
+    
+    //angle commands
+    void change_pitch();
+    void change_yaw();
+    void change_roll();
+    
+    
+private:
+    //Model Pointer
+    Model *sub_model;
+    Mode *current_mode;
+    string command;
+    
+    //Variables for thrust and angles
+    float x_thrust, y_thrust, z_thrust;
+    float pitch, yaw, roll;
+    
+    void start_motor0();
+    void start_motor1();
+    void start_motor2();
+    void start_motor3();
+    void start_motor4();
+    void start_motor5();
+    void start_motor6();
+    void start_motor7();
+    
 };
 
-#endif /* CONTROLLER_H_ */
+
+
+#endif /* SRC_CONTROLLER_H_ */
