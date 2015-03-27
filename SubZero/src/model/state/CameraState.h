0fea9ca7fbc657f83686859002d6856cb6ac43ae
James534 <james534@outlook.com>
2015-03-26 23:42:25 -0400

    code stubbed camera state and fpga state

--- SubZero/src/model/state/CameraState.h
+++ SubZero/src/model/state/CameraState.h
@@ -2,21 +2,36 @@
  * CameraState.h
  *
  *  Created on: Feb 7, 2015
- *      Author: carl
+ *      Author: carl, james
  */
 
 #ifndef CAMERASTATE_H_
 #define CAMERASTATE_H_
 #include "Observable.h"
+#include "ImgData.h"	//rename it to whatever its called
+
+#include <list>
+#include <vector>
 
 /**
  * CameraState is an Observable used by CameraModel to hold data sent from cameras.
- * It can notify any Observers when changes are made.
  */
 
 class CameraState : public Observable {
 private:
-	double myState;
+
+	//boolean to signal if a new frame has started
+	bool frameStarted;
+
+	//the max length of the linked list
+	//the amount of frames the state will hold
+	int maxLength;
+
+	// LinkedList <Vector<ImgData>>
+	// Linked list to hold the states for every frame
+	// Vector to hold the different states for each frame, filtered and non-filtered
+	// not sure if this is how you initalize a linked list of vectors
+	std::list<std::vector<ImgData>> stateData;
 
 public:
 	/**
@@ -29,8 +44,53 @@ public:
 	 */
 	virtual ~CameraState();
 
-	double getState();
-	void setState(myState);
+	/**
+	 * Starts a new Frame
+	 * Have to be called before you can call setState()
+	 * Only Model should be calling this function (might implement a check for that)
+	 */
+	void startFrame();
+
+	/**
+	 * Ends the frame
+	 * Have to be called after startFrame() is called
+	 * If called before startFrame, then it does nothing
+	 * Be careful when calling this, since you cannot change the frame after it has ended
+	 */
+	void endFrame();
+
+	/**
+	 * Sets the state
+	 * SHOULD ONLY BE CALLED AFTER startFrame() IS CALLED
+	 * OR ELSE IT WOULD THROW FrameNotStartedException
+	 * @param image data to be set for this frame
+	 * @throws FrameNotStartedException
+	 */
+	void setState(ImgData data);
+
+	/**
+	 * Returns a deep copy of an image specified with the _id_ at _t_ frames before this call
+	 * @param id = id of the image that is needed
+	 			t = how many frames ago was the image stored
+	 */
+	ImgData getState (std::string id, int t);
+
+	/**
+	 * Returns a deep copy of the latest image specified with the _id_ 
+	 * (same as calling getImage(id, 0))
+	 * @param id = id of the image that is needed
+	 */
+	ImgData getState (std::string id);
+
+	/**
+	 * Gets the newest raw image
+	 */
+	ImgData getRaw();
+
+	/**
+	 * Gets the raw image _t_ frames before
+	 */
+	ImgData getRaw(int t);
 
 };
 
--- SubZero/src/model/state/FPGAState.h
+++ SubZero/src/model/state/FPGAState.h
@@ -2,21 +2,34 @@
  * FPGAState.h
  *
  *  Created on: Feb 7, 2015
- *      Author: carl
+ *      Author: carl, james
  */
 
 #ifndef FPGASTATE_H_
 #define FPGASTATE_H_
 #include "Observable.h"
+#include "FPGAData.h"
+
+#include <list>
+#include <vector>
 
 /**
  * FPGAState is an Observable used by FPGAModel to hold data sent from the FPGA.
- * It can notify any Observers when changes are made.
  */
 
 class FPGAState : public Observable {
 private:
-	double myState;
+
+	//boolean to signal if a new frame has started
+	bool frameStarted;
+
+	//the length of the linked list
+	//The amount of frames that the state will hold
+	int maxLength;
+
+	//Same structure as the camData,
+	//LinkedList <Vector<ImgData>>
+	std::list<std::vector<FPGAData>> stateData;
 
 public:
 	/**
@@ -29,9 +42,48 @@ public:
 	 */
 	virtual ~FPGAState();
 
-	double getState();
-	void setState(double newState);
 
+	/**
+	 * Starts a new Frame
+	 * Have to be called before you can call setState()
+	 * Only Model should be calling this function (might implement a check for that)
+	 */
+	void startFrame();
+
+	/**
+	 * Ends the frame
+	 * Have to be called after startFrame() is called
+	 * If called before startFrame, then it does nothing
+	 * Be careful when calling this, since you cannot change the frame after it has ended
+	 */
+	void endFrame();
+
+	/**
+	 * Gets the most recent fpga state
+	 * (Need discussion on whether FPGA data will need an ID)
+	 */
+	FPGAData getState();
+
+	/**
+	 * Gets the fpga state _t_ frames before
+	 */
+	FPGAData getState(int t);
+
+	/**
+	 * Sets the fpga state
+	 * Should be called once every frame
+	 */
+	void setState(FPGAData state);
+
+	/**
+	 * Gets the most recent raw FPGA data
+	 */
+	rawFPGA getRaw();
+
+	/**
+	 * Gets the raw FPGA data 'i' frames before this one
+	 */
+	rawFPGA getRaw(int t);
 
 };
 
