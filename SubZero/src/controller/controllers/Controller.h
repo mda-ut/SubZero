1d03ec68442f6e615c95e3e72c984dae14ddf86b
cannon-edgar <edgarwang95@gmail.com>
2015-03-28 20:52:54 -0400

    stubbed command classes
    d

--- SubZero/src/controller/controllers/Controller.h
+++ SubZero/src/controller/controllers/Controller.h
@@ -13,6 +13,11 @@
 
 using namespace std;
 
+//Mode not needed
+//we have to find out what the command is
+//controller only does event handling -> list of if statements determining what to excute
+//command structure & task class
+
 class Controller{
 public:
     //constructor for the controller class
@@ -23,40 +28,23 @@ public:
     }
     //Destructor to free pointers
     ~Controller(){
-        free(sub_model);
-        free(current_mode);
+        //free(sub_model);
+        //free(current_mode);
     }
+
+    //need a handle event function
+
     
-    //movement commands
-    void move_forward();
-    void move_backward();
-    void move_right();
-    void move_left();
-    
-    //angle commands
-    void change_pitch();
-    void change_yaw();
-    void change_roll();
-    
-    
+
 private:
     //Model Pointer
     Model *sub_model;
     Mode *current_mode;
     string command;
     
-    //Variables for thrust and angles
+    //Variables for thrust an[d angles
     float x_thrust, y_thrust, z_thrust;
     float pitch, yaw, roll;
-    
-    void start_motor0();
-    void start_motor1();
-    void start_motor2();
-    void start_motor3();
-    void start_motor4();
-    void start_motor5();
-    void start_motor6();
-    void start_motor7();
     
 };
 
--- SubZero/src/controller/task/Task.h
+++ SubZero/src/controller/task/Task.h
@@ -12,6 +12,27 @@ class Task {
 public:
 	Task();
 	virtual ~Task();
+
+	//movement commands
+    void move_forward();
+    void move_backward();
+    void move_right();
+    void move_left();
+    
+    //angle commands
+    void change_pitch();
+    void change_yaw();
+    void change_roll();
+    
+ private:
+   	void start_motor0();
+    void start_motor1();
+    void start_motor2();
+    void start_motor3();
+    void start_motor4();
+    void start_motor5();
+    void start_motor6();
+    void start_motor7();
 };
 
 #endif /* TASK_H_ */
