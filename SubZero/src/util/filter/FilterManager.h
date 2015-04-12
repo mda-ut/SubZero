d0d414a953699a685f3ee3796fc48d4054067a6a
ahsueh1996 <ahsueh1996@gmail.com>
2015-04-11 20:50:14 -0400

    --------------------------------- IDHasher.h FilterManager.h Filter.h RGBFilter.h FilterFactory.h --------------------------------- Stubs altered: java doc convention according to Apr. 7 model meeting

--- SubZero/src/util/filter/FilterManager.h
+++ SubZero/src/util/filter/FilterManager.h
@@ -14,6 +14,10 @@
 #include "DataFactory.h"
 #include <vector>
 
+/*
+ * NodeData struct class for IDHasher. this defines the type of data
+ * contained by each node in the IDHasher.
+ */
 struct NodeData {
 	Filter* filter;
 };
@@ -34,176 +38,6 @@ struct NodeData {
  * the user can then use to refer to the filters.
  */
 class FilterManager {
-
-public:
-	/* ==========================================================================
-	 * CONSTRUCTOR AND DESCONSTRUCTOR
-	 * ==========================================================================
-	 */
-
-	/**
-	 * FilterManager constructor initializes a new FilterManager object for
-	 * use. The data processed by this FM will be tagged with the FM's ID.
-	 *
-	 * @param Identifier of the FilterManager, data processed by this FM will have this ID. (ID is recommended to reflect FM's function and is unique! Uniqueness is a feature only for when the FMs are processing data to store in state, thus FM won't enforce ID uniqueness.)
-	 */
-	FilterManager(std::string fmID);
-
-	/**
-	 * FilterManager constructor overload allows user to initiate a FM that
-	 * will do automatic IDing of the filters it manages. In comparison, the
-	 * default class allows customized IDing.
-	 *
-	 * @param Identifier of the FilterManager, data processed by this FM will have this ID. (ID is recommended to reflect FM's function and is unique! Uniqueness is a feature only for when the FMs are processing data to store in state, thus FM won't enforce ID uniqueness.)
-	 * @param int 1 for automatic IDing. 0 for default.
-	 */
-	FilterManager(std::string fmID, int type);
-
-	virtual ~FilterManager();
-
-	/* ==========================================================================
-	 * MAIN FUNCTIONALITY: FILTRATION
-	 * ==========================================================================
-	 */
-
-	/**
-	 * The filter() function accepts a raw data input. Depending on
-	 * what kind of model it belongs to, it takes in a image or FPGA
-	 * data and filters it according to the current filter chain.
-	 *
-	 * @param in refers to a pointer to the image being filtered.
-	 * @param out refers to the pointer that will receive the filtered data.
-	 * @return int 0 for success, 1 for there is some error in filtering.
-	 */
-	int filter(Data* in, Data** out);
-
-
-	/* ==========================================================================
-	 * FILTER MANAGMENT FUNCTIONS: CUSTOM ID MODE
-	 * ==========================================================================
-	 */
-
-	/**
-	 * Inserts a Filter class object to the back of the chain. Note: ID
-	 * initialization is required here rather than at the filter object
-	 * level because 1. filters don't need an ID to function and 2. If
-	 * ID is given to FM, it helps FM to maintain uniqueness of IDs.
-	 *
-	 * @param ID is the unique identifier chosen by the owner of the filter.
-	 * @param filter, a pointer to a filter object.
-	 * @return 0 for success, 1 for ID is not unique.
-	 */
-	int insertFilter(std::string filterID, Filter* filter);
-
-	/**
-	 * Inserts a Filter class object in front of a specific target
-	 * filter (by ID).
-	 *
-	 * @param ID of the new filter.
-	 * @param filter, a pointer to a filter object to be inserted.
-	 * @param targetID, the location to perform insertion. BEGIN and END keywords are also recognized.
-	 * @return 0 for success, 1 for ID is not unique, 2 for target not found.
-	 */
-	int insertFilter(std::string filterID, Filter* filter, std::string targetID);
-
-	/**
-	 * Replaces a particular filter, identified by ID, in the chain with
-	 * another. If ID not found, function will NOT perform insertion and
-	 * will instead return the appropriate error code.
-	 *
-	 * @param ID of the new filter.
-	 * @param filter, a pointer to a filter object that will be inserted.
-	 * @param targetID, the old filter to replace.
-	 * @return 0 for success, 1 for ID not unique, 2 for targetID not found.
-	 */
-	int replaceFilter(std::string filterID, Filter* filter, std::string targetID);
-
-	/**
-	 * Deletes the first occurrence of a filter from filterChain by ID.
-	 *
-	 * @param targetID of the filter to delete. BEGIN and END keywords accepted.
-	 * @return 0 for success, 2 for targetID not found.
-	 */
-	int deleteFilter(std::string targetID);
-
-	/**
-	 * Deletes all filters in chain.
-	 *
-	 * @return 0 for success.
-	 */
-	int deleteFilterChain();
-
-	/* ==========================================================================
-	 * FILTER MANAGEMENT: AUTOMATIC ID MODE
-	 * Auto mode is not recommended since the elements in the filter
-	 * chain can get to be very ambiguous.
-	 * ==========================================================================
-	 */
-
-	/**
-	 * Insertion to back under automatic IDing mode.
-	 *
-	 * @param filter, a pointer to a filter object.
-	 * @return 0 for success, 1 for ID is not unique.
-	 */
-	int insertFilter(Filter* filter);
-
-	/**
-	 * Inserts a Filter class object in front of a specific target
-	 * filter (by ID). This is the overload function under automatic ID
-	 * mode.
-	 *
-	 * @param filter, a pointer to a filter object to be inserted.
-	 * @param targetID, the location to perform insertion. FRONT and BACK keywords are also recognized.
-	 * @return 0 for success, 1 for ID is not unique, 2 for target not found.
-	 */
-	int insertFilter(Filter* filter, std::string targetID);
-
-	/**
-	 * Replacement under automatic IDing mode.
-	 * If targetID not found, function will NOT perform insertion and
-	 * will instead return the appropriate error code.
-	 *
-	 * @param filter, a pointer to a filter object that will be inserted.
-	 * @param targetID, the old filter to replace.
-	 * @return 0 for success, 1 for ID not unique, 2 for targetID not found.
-	 */
-	int replaceFilter(Filter* filter, std::string targetID);
-
-	/* ==========================================================================
-	 * SUPPLAMENTS
-	 * ==========================================================================
-	 */
-
-	/**
-	 * Reports the number of filters in filterChain.
-	 *
-	 * @return number of filters.
-	 */
-	int getSizeOfFilter();
-
-	/**
-	 * For a given ID, the filterChain is searched and the index of the
-	 * filter object with the specific ID is identified and returned
-	 *
-	 * @param ID represents the filter object to look for
-	 * @return int of the index value from 0 ~ chain size-1, -1 for no match.
-	 */
-	int getIndexByID(std::string ID);
-
-	/**
-	 * Reports a copy of the vector string of filter IDs in filterChain.
-	 *
-	 * @return vector of strings
-	 */
-	std::vector<std::string> getFilterChainIDs();
-
-	/**
-	 * Report the ID of the FM.
-	 *
-	 * @return string fmID
-	 */
-	std::string getFMID();
 
 private:
 	/* ==========================================================================
@@ -233,6 +67,176 @@ private:
 	 * Identifier for the FM
 	 */
 	std::string fmID;
+
+public:
+	/* ==========================================================================
+	 * CONSTRUCTOR AND DESCONSTRUCTOR
+	 * ==========================================================================
+	 */
+
+	/**
+	 * FilterManager constructor initializes a new FilterManager object for
+	 * use. The data processed by this FM will be tagged with the FM's ID.
+	 *
+	 * @param fmID	Identifier of the FilterManager, data processed by this FM will have this ID. (ID is recommended to reflect FM's function and is unique! Uniqueness is a feature only for when the FMs are processing data to store in state, thus FM won't enforce ID uniqueness.)
+	 */
+	FilterManager(std::string fmID);
+
+	/**
+	 * FilterManager constructor overload allows user to initiate a FM that
+	 * will do automatic IDing of the filters it manages. In comparison, the
+	 * default class allows customized IDing.
+	 *
+	 * @param fmID	Identifier of the FilterManager, data processed by this FM will have this ID. (ID is recommended to reflect FM's function and is unique! Uniqueness is a feature only for when the FMs are processing data to store in state, thus FM won't enforce ID uniqueness.)
+	 * @param 		1 for automatic IDing. 0 for default.
+	 */
+	FilterManager(std::string fmID, int type);
+
+	virtual ~FilterManager();
+
+	/* ==========================================================================
+	 * MAIN FUNCTIONALITY: FILTRATION
+	 * ==========================================================================
+	 */
+
+	/**
+	 * This function accepts a raw data input. Depending on
+	 * what kind of model it belongs to, it takes in a image or FPGA
+	 * data and filters it according to the current filter chain.
+	 *
+	 * @param in 	refers to a pointer to the image being filtered.
+	 * @param out 	refers to the pointer that will receive the filtered data.
+	 * @return 		0 for success, 1 for there is some error in filtering.
+	 */
+	int applyFilterChain(Data* in, Data** out);
+
+
+	/* ==========================================================================
+	 * FILTER MANAGMENT FUNCTIONS: CUSTOM ID MODE
+	 * ==========================================================================
+	 */
+
+	/**
+	 * Inserts a Filter class object to the back of the chain. Note: ID
+	 * initialization is required here rather than at the filter object
+	 * level because 1. filters don't need an ID to function and 2. If
+	 * ID is given to FM, it helps FM to maintain uniqueness of IDs.
+	 *
+	 * @param ID 		is the unique identifier chosen by the owner of the filter.
+	 * @param filter	the pointer to a filter object.
+	 * @return 			0 for success, 1 for ID is not unique.
+	 */
+	int insertFilter(std::string filterID, Filter* filter);
+
+	/**
+	 * Inserts a Filter class object in front of a specific target
+	 * filter (by ID).
+	 *
+	 * @param ID 		of the new filter.
+	 * @param filter 	the a pointer to a filter object to be inserted.
+	 * @param targetID 	the location to perform insertion. BEGIN and END keywords are also recognized.
+	 * @return 			0 for success, 1 for ID is not unique, 2 for target not found.
+	 */
+	int insertFilter(std::string filterID, Filter* filter, std::string targetID);
+
+	/**
+	 * Replaces a particular filter, identified by ID, in the chain with
+	 * another. If ID not found, function will NOT perform insertion and
+	 * will instead return the appropriate error code.
+	 *
+	 * @param ID 		of the new filter.
+	 * @param filter 	the pointer to a filter object that will be inserted.
+	 * @param targetID	the old filter to replace.
+	 * @return 			0 for success, 1 for ID not unique, 2 for targetID not found.
+	 */
+	int replaceFilter(std::string filterID, Filter* filter, std::string targetID);
+
+	/**
+	 * Deletes the first occurrence of a filter from filterChain by ID.
+	 *
+	 * @param targetID 	of the filter to delete. BEGIN and END keywords accepted.
+	 * @return 			0 for success, 2 for targetID not found.
+	 */
+	int deleteFilter(std::string targetID);
+
+	/**
+	 * Deletes all filters in chain.
+	 *
+	 * @return 0 for success.
+	 */
+	int deleteFilterChain();
+
+	/* ==========================================================================
+	 * FILTER MANAGEMENT: AUTOMATIC ID MODE
+	 * Auto mode is not recommended since the elements in the filter
+	 * chain can get to be very ambiguous.
+	 * ==========================================================================
+	 */
+
+	/**
+	 * Insertion to back under automatic IDing mode.
+	 *
+	 * @param filter 	is a pointer to a filter object.
+	 * @return 			0 for success, 1 for ID is not unique.
+	 */
+	int insertFilter(Filter* filter);
+
+	/**
+	 * Inserts a Filter class object in front of a specific target
+	 * filter (by ID). This is the overload function under automatic ID
+	 * mode.
+	 *
+	 * @param filter 	is a pointer to a filter object to be inserted.
+	 * @param targetID	is the location to perform insertion. FRONT and BACK keywords are also recognized.
+	 * @return 			0 for success, 1 for ID is not unique, 2 for target not found.
+	 */
+	int insertFilter(Filter* filter, std::string targetID);
+
+	/**
+	 * Replacement under automatic IDing mode.
+	 * If targetID not found, function will NOT perform insertion and
+	 * will instead return the appropriate error code.
+	 *
+	 * @param filter	is a pointer to a filter object that will be inserted.
+	 * @param targetID  is the old filter to replace.
+	 * @return 			0 for success, 1 for ID not unique, 2 for targetID not found.
+	 */
+	int replaceFilter(Filter* filter, std::string targetID);
+
+	/* ==========================================================================
+	 * SUPPLEMENTS
+	 * ==========================================================================
+	 */
+
+	/**
+	 * Reports the number of filters in filterChain.
+	 *
+	 * @return number of filters.
+	 */
+	int getSizeOfFilter();
+
+	/**
+	 * For a given ID, the filterChain is searched and the index of the
+	 * filter object with the specific ID is identified and returned
+	 *
+	 * @param ID 	represents the filter object to look for
+	 * @return 		int of the index value from 0 ~ chain size-1, -1 for no match.
+	 */
+	int getIndexByID(std::string ID);
+
+	/**
+	 * Reports a copy of the vector string of filter IDs in filterChain.
+	 *
+	 * @return vector of strings
+	 */
+	std::vector<std::string> getFilterChainIDs();
+
+	/**
+	 * Report the ID of the FM.
+	 *
+	 * @return string fmID
+	 */
+	std::string getFMID();
 };
 
 #endif /* FILTERMANAGER_H_ */
