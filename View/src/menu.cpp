c9b7869b92fda14960601d67bb719683eaa9bf8b
pereodinofzolos <edem4420@hotmail.com>
2015-05-02 17:48:09 -0400

    Added Signals and slots
    Filled in skeleton and base for future GuiView implementation

--- View/src/menu.cpp
+++ View/src/menu.cpp
@@ -3,17 +3,52 @@
 February 7th 2015
 
 */
-
 #include "menu.h"
-#include "guiview.h"
+#include <QDebug>
 
+Menu::Menu(QWidget *parent) :
+    QWidget(parent)
+{
+    /** @param Selector This item holds all views.
+     * It shall be replaced in the future probably
+     * with list widget or tree widget.
+     *
+     * @param affirmButton This button is used to
+     * confirm the desired instantiation of a view.
+     */
 
-Menu::Menu(){
-  
+    selector = new QComboBox;
+    affirmButton = new QPushButton;
+    horizontal1 = new QHBoxLayout;
+
+    //Initializing the layout
+    selector->addItem("GUI");
+    selector->addItem("SIM");
+
+    affirmButton->setText("Ok");
+
+    horizontal1->addWidget(selector);
+    horizontal1->addWidget(affirmButton);
+
+    this->setLayout(horizontal1);
+
+    //Connecting Signals and Slots
+    connect(affirmButton, SIGNAL(clicked()),SLOT(makeView()));
+
 }
 
+/**
+ * @param makeView Instantiates the current selected view
+ */
 
-Menu::~Menu(){
-  
-  
+void Menu::makeView()
+{
+    if (selector->currentText() == "GUI")
+    {
+        qDebug()<< "Initiating GUI";
+    }
+    else if (selector->currentText() == "SIM")
+    {
+       qDebug()<< "Initiating Simulation mode";
+    }
 }
