c9793ec0ca3a25b215db89168943b376d7bbdca7
pereodinofzolos <edem4420@hotmail.com>
2015-05-02 17:55:47 -0400

    started graphical implementation
    Reformatted views to use QWidget, resulting in removal of destructor and setup of graphical implementation
    Comments for functions are in the headers of the abstract classes in which they were first declared
    More work is required before the updates can be used to display the images as intended but the necesary items are all present and the conversion from opencv sources to pixmaps in Qt (based on models initial mode of view communication) is working as intended for rgb maps (not yet rgba).

--- View/src/guiview.cpp
+++ View/src/guiview.cpp
@@ -6,17 +6,44 @@
  *      Author: edem
  */
 
+#ifndef GUIVIEW_H
+#define GUIVIEW_H
 
 #include "view.h"
+#include "observer.h"
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QPixmap>
+#include <QPaintEvent>
+#include <QGridLayout>
+#include <QWidget>
+#include <cv.h>
+#include <highgui.h>
 
-GuiView::GuiView() {
 
-}
 
-int GuiView::sendToC (char *viewID, char* userInput){
+/** The GuiView class creates a GUI
+ * to allow interaction with the
+ * controller.
+ */
 
-}
 
-GuiView::~GuiView(){
+class GuiView : public View, public Observer
+{
+public:
+    GuiView();
 
-}
+    QGraphicsView *viewFront;
+    QGraphicsView *viewDown;
+    QGraphicsScene *scene;
+    QPixmap *pixmap;
+    QGridLayout *mainLayout;
+    QPaintEvent *paintEvent;
+
+
+    QPixmap *makeQPixmap(cv::Mat* imgData);
+    void displayUpdate (cv::Mat* img, QGraphicsView *view);
+    void update(cv::Mat* imgLocFront,cv::Mat* imgLocDown , int* sonarLoc);
+};
+
+#endif // GUIVIEW_H
