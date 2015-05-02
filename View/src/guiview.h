0379b1c89969aa6a8c9e74d6b0c1920d23873e4c
pereodinofzolos <edem4420@hotmail.com>
2015-05-02 17:51:24 -0400

    started graphical implementation

--- View/src/guiview.h
+++ View/src/guiview.h
@@ -1,52 +1,44 @@
 #ifndef GUIVIEW_H_
 #define GUIVIEW_H_
 
-#include <QComboBox>
-#include <QPushbutton>
+#ifndef GUIVIEW_H
+#define GUIVIEW_H
+
+#include "view.h"
+#include "observer.h"
+#include <QGraphicsView>
 #include <QGraphicsScene>
+#include <QPixmap>
+#include <QPaintEvent>
+#include <QGridLayout>
+#include <QWidget>
+#include <cv.h>
+#include <highgui.h>
 
 
 
-
-/**
- * This child class of view uses the mouse and a graphic
- * user interface to pass user information to controller
+/** The GuiView class creates a GUI
+ * to allow interaction with the
+ * controller.
  */
 
 
-class GuiView: public View
+class GuiView : public View, public Observer
 {
-	Q_OBJECT
+public:
+    GuiView();
 
-public: GuiView();
-
-
-/**
- * The combo box holds actions. When the user selects an action and
- * confirms it via the accompanying confirmation button the
- * signal activates a slot that sends the text id of the view
- * and the requested action to controller. As long as the button
- * is held down the action will be continuously carried out.
- */
-
-		QComboBox actionBox;
-		QGraphicsScene scene;
-		QPushButton actionButton;
-		QPushButton closeButton;
+    QGraphicsView *viewFront;
+    QGraphicsView *viewDown;
+    QGraphicsScene *scene;
+    QPixmap *pixmap;
+    QGridLayout *mainLayout;
+    QPaintEvent *paintEvent;
 
 
+    QPixmap *makeQPixmap(cv::Mat* imgData);
+    void displayUpdate (cv::Mat* img, QGraphicsView *view);
+    void update(cv::Mat* imgLocFront,cv::Mat* imgLocDown , int* sonarLoc);
+};
 
-/*		Actions
- * 		void moveForward(QMouseEvent *mousemot);
-		void moveBackward(QMouseEvent *mousemot);
-		void moveLeft(QMouseEvent *mousemot);
-		void moveRight(QMouseEvent *mousemot);
-		void moveUp(QMouseEvent *mousemot);
-		void moveDown(QMouseEvent *mousemot);
-		void surface(QMouseEvent *mousemot);
-		void launchTor(QMouseEvent *mousemot);
-		void launchGra(QMouseEvent *mousemot);
-*/
-		virtual ~GuiView();
-}
-#endif /* GUIVIEW_H_ */
+#endif // GUIVIEW_H
