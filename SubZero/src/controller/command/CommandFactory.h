67ef067aca4173b1272273279f5148199b9784fc
LuckyChewie <chewie.decaria@mail.utoronto.ca>
2015-05-02 20:35:15 -0400

    Editted Controller and ControllerThread and began to work on CommandFactory - did not get far but had to commit in order to accept Edgar's Code.

---
+++ SubZero/src/controller/command/CommandFactory.h
@@ -0,0 +1,27 @@
+/*
+ * CommandFactory.h
+ *
+ *  Created on: Mar 29, 2015
+ *      Author: avcd
+ */
+
+#ifndef COMMANDFACTORY_H_
+#define COMMANDFACTORY_H_
+
+#include <string>
+#include "Command.h"
+
+class CommandFactory {
+	public:
+		/**
+		* An postToScreen Command is created.
+		*
+		* @param postToScreenTask for the
+		* @param postString for the string to be posted
+		* @return Command object pointer
+		*/
+		static Command* createPostToScreen(Task postToScreen, string PostString);
+
+};
+
+#endif /* COMMANDFACTORY_H_ */
