6759cd0f8209d4025235916d8453d5e9e056f44b
freshnewegg <edgarwang95@gmail.com>
2015-05-09 22:42:41 -0400

    fixed space

---
+++ SubZero/src/controller/task/TouchBuoy.cpp
@@ -0,0 +1,24 @@
+/*
+ * TouchBuoy.cpp
+ *
+ *  Created on: May 9, 2015
+ *      Author: ed
+ */
+
+#include "TouchBuoy.h"
+
+namespace std {
+
+TouchBuoy::TouchBuoy() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TouchBuoy::~TouchBuoy() {
+	// TODO Auto-generated destructor stub
+}
+
+void TouchBuoy::execute(){
+
+}
+ /* namespace std */
