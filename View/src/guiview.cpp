0b992766ed44b48eb6b70d127d09de888c39eb30
pereodinofzolos <edem4420@hotmail.com>
2015-05-14 00:21:00 -0400

    Added sizing and layout, fixed displayUpdate

--- View/src/guiview.cpp
+++ View/src/guiview.cpp
@@ -1,43 +1,95 @@
-
 #include "guiview.h"
 #include <QImage>
+#include <QString>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+
 
 GuiView::GuiView()
 {
-    viewFront = new QGraphicsView;
-    viewDown = new QGraphicsView;
-    scene = new QGraphicsScene;
-    pixmap = new QPixmap;
-    mainLayout = new QGridLayout;
+    actionSelector = new QComboBox;
+    affirmButton = new QPushButton;
+
+
+    sceneFront = new QGraphicsScene;
+    sceneDown = new QGraphicsScene;
+    viewFront = new QGraphicsView(sceneFront);
+    viewDown = new QGraphicsView(sceneDown);
+    viewFront->show();
+    viewDown->show();
+
+
+
+    sonarStrength = new QLabel("Sonar Strength");
+    displaySize = new QSize(800,600);
+    pixmap = new QPixmap(*displaySize);
+
+
+    //Set the size of the scenes and views to the displaySize
+    //Front camera is the first (leftmost) camera
+
+    sceneFront->setSceneRect(0,0,displaySize->width(),displaySize->height());
+    sceneDown->setSceneRect(displaySize->width(),displaySize->height(),displaySize->width(),displaySize->height());
+    viewFront->setFixedSize(*displaySize);
+    viewDown->setFixedSize(*displaySize);
+
+
+    //Setup the layout
+    QHBoxLayout *viewLayout = new QHBoxLayout;
+    viewLayout->addWidget(viewFront);
+    viewLayout->addWidget(viewDown);
+
+    QHBoxLayout *selectorLayout = new QHBoxLayout;
+    selectorLayout->addWidget(actionSelector);
+    selectorLayout->addWidget(affirmButton);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addLayout(viewLayout);
+    mainLayout->addLayout(selectorLayout);
+    mainLayout->addWidget(sonarStrength);
 
 }
 
-QPixmap *GuiView::makeQPixmap(cv::Mat *imgData)
+
+
+QPixmap GuiView::makeQPixmap(cv::Mat *imgData)
 {
-    QPixmap *p=0;
-
     // http://asmaloney.com/2013/11/code/converting-between-cvmat-and-qimage-orqpixmap
+    //Assuming rgb, need to add a check for other formats and null.
 
-    //Assuming rgba, need to add a check for other formats and null.
+    QPixmap p(*displaySize);
 
     QImage image(imgData->data, imgData->cols, imgData->rows,
                  imgData->step, QImage::Format_RGB32);
 
-   p->fromImage(image);
-   return p;
+    p.fromImage(image);
+    return p;
 }
 
-void GuiView::displayUpdate(cv::Mat *img, QGraphicsView *view)
+
+
+
+void GuiView::displayUpdate(cv::Mat *img,QGraphicsScene* scene)
 {
+
+    //Create the new image
+    //then clear the scene so the screen doesn't have a large 'load time' as you replace the image
+
+    *pixmap = makeQPixmap(img);
     scene->clear();
-    pixmap = makeQPixmap(img);
     scene->addPixmap(*pixmap);
-    view->setScene(scene);
-    view->show();
 }
+
+
+
 
 void GuiView::update(cv::Mat *imgLocFront, cv::Mat *imgLocDown, int *sonarLoc)
 {
-    displayUpdate(imgLocFront,viewFront);
-    displayUpdate(imgLocDown, viewDown);
+    displayUpdate(imgLocFront,sceneFront);
+    displayUpdate(imgLocDown,sceneDown);
+    sonarStrength->setText("Sonar strength"+ QString::number(*sonarLoc));
+
+
 }
+
+
