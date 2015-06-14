4dcebb378bbd13c0be0052bc31d896d87cea1df8
freshnewegg <edgarwang95@gmail.com>
2015-06-13 21:53:49 -0400

    fixed logger bug

--- SubZero/src/util/Util.cpp
+++ SubZero/src/util/Util.cpp
@@ -16,3 +16,10 @@ Util::~Util() {
 	// TODO Auto-generated destructor stub
 }
 
+
+std::string Util::getWorkingDirectory( void ) {
+   char buff[100];
+   getcwd( buff, 100 );
+   std::string cwd( buff );
+   return cwd;
+}
--- SubZero/src/util/Util.h
+++ SubZero/src/util/Util.h
@@ -8,10 +8,19 @@
 #ifndef UTIL_H_
 #define UTIL_H_
 
+#include <string>
+#include <unistd.h>
+
 class Util {
 public:
 	Util();
 	virtual ~Util();
+
+    /**
+     * Gets the Working Directory
+     * @return A string of the working directory
+     */
+    static std::string getWorkingDirectory( void );
 };
 
 #endif /* UTIL_H_ */
