5099b709baf6bb69e452763425da0c3c61a6381d
Pereodinofzolos <edem4420@hotmail.com>
2015-06-26 20:15:09 -0400

    this better work -Carl

--- SubZero/src/view/view.h
+++ SubZero/src/view/view.h
@@ -1,55 +1,72 @@
 #ifndef VIEW_H
 #define VIEW_H
 
-#include <QGraphicsView>
-#include <QGraphicsScene>
-#include <QPixmap>
-#include <QSize>
 #include <QWidget>
-#include <QLabel>
+#include <QRect>
+#include <QImage>
+#include <QPainter>
+#include <QPaintEvent>
+//#include "observer.h"    //Need a concerte observer class
 #include <cv.h>
 #include <highgui.h>
 
-/** This abstract class holds the basic
- * properties expected of all views -a
- * display via QGraphics, QPixmap;
- * and Signals and slots
+
+using namespace cv;
+
+
+/**
+ * Updates the view class by using painter.drawImage (in paintEvent) to
+ * display the camera images as a lower quality alternative
+ * to the pixmap QGraphics Scene method. This is preferable
+ * as the previous method was largely resource intensive and
+ * consisted of more steps.
  *
+ *
+ * This class is responsible for the setting up the image. The data for the
+ * views (with the exception of autonomous) is handled in the Observer
+ * class.
  */
 
-class View :public QWidget
+
+
+
+/**
+  * @param frontCameraRect  Holds the rectangle in which the image from the front
+  * camera is drawn
+  * @param downCameraRect  Holds the rectangle in which the image from the downwards
+  * camera is drawn
+  *
+  * @param painter Performs all the painting on the window of the view through
+  * the paintEvent function/routine
+  *
+  * @param makeQImage used to display the image on the views
+  *
+  */
+
+//Ignoring the inheritance from observer until I place this back in view
+
+class View: public QWidget
 {
     Q_OBJECT
+
 public:
     View();
 
-    /**
-    * @param viewFront 'Screen' of the Front facing camera
-    * @param viewDown 'Screen' of the Downwards facing camera
-    * @param sceneFront the image to be shown in viewFront
-    * @param sceneDown the image to be shown in viewDown
-    * @param displaySize the size of the view screens and their scenes
-    *
-    *
-    */
+    QImage *frontCameraImage;
+    QImage *downCameraImage;
 
-    QGraphicsView *viewFront;
-    QGraphicsView *viewDown;
-    QGraphicsScene *sceneFront;
-    QGraphicsScene *sceneDown;
-    QPixmap *pixmap;
-    QSize *displaySize;
-    QLabel *sonarStrength;
+private:
+    void initializeView();
+    QRect *frontCameraRect;
+    QRect *downCameraRect;
 
-    /**
-     * makeQPixmap converts the opencv Mat
-     * object into a QPixmap.
-     * displayImage updates the scene with a
-     * pixmap to be displayed
-     */
+    QPainter *painter;
 
-    virtual QPixmap makeQPixmap(cv::Mat* img)=0;
-    virtual void displayUpdate (cv::Mat* img, QGraphicsScene *scene)= 0;
+    void paintEvent(QPaintEvent *event); //All drawing and painting will take place in here
+
+    //From View
+    virtual void makeQImage(Mat *imgData, QImage *imgHolder)=0; //Converting Mat to QImage //Creating camera image, overload
+
 };
 
-#endif // VIEW_H
+#endif // VIEW2_H
