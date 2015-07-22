525f9546f655676ba1c67ab3a7ef065928f86ba6
James534 <james534@outlook.com>
2015-07-22 18:33:10 -0400

    Added portal tasak

---
+++ SubZero/src/controller/task/PortalTask.cpp
@@ -0,0 +1,7 @@
+#include "PortalTask.h"
+
+PortalTask::PortalTask()
+{
+
+}
+
---
+++ SubZero/src/controller/task/PortalTask.h
@@ -0,0 +1,11 @@
+#ifndef PORTALTASK_H
+#define PORTALTASK_H
+
+
+class PortalTask
+{
+public:
+    PortalTask();
+};
+
+#endif // PORTALTASK_H
