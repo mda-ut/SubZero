c2a3f37d3c933d40d929458cb2debafa48ab7d20
James534 <james534@outlook.com>
2015-07-23 03:30:37 -0400

    Added property files -improved buoy task with distance code

--- SubZero/src/controller/task/BuoyTask.h
+++ SubZero/src/controller/task/BuoyTask.h
@@ -11,8 +11,19 @@ public:
 
     int task();
     void execute();
+
 private:
     CameraModel* camModel;
-};
+    Properties* settings;
+    int imgWidth = -1;
+    int imgHeight = -1;
+    int travelDist;
+    float deltaAngle = -1;
 
+    void move(float d);
+    void rise(float h);
+    void rotate(float t);
+    void slide(float d);
+    float calcDistance(float rad);
+};
 #endif // BUOYTASK_H
